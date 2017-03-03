bfb816838747ebb686fb04b7be64074620573035
Joe Querin <joecue@gmail.com>
2017-03-03 09:55:29 -0500

    Update firebase-init.js for Production Firebase Instance

---
+++ js/firebase-init-dev.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD1FZh-KK_FJDACf6mdburwccgxPGIkcqg",
+    authDomain: "lorainccc-39418.firebaseapp.com",
+    databaseURL: "https://lorainccc-39418.firebaseio.com",
+    storageBucket: "lorainccc-39418.appspot.com",
+    messagingSenderId: "241528039156"
+  };
+  firebase.initializeApp(config);
